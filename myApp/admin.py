cea8c4f1a43847d8d4ece1584288c75f4aa53ba9
Pritom <pritom.cse.29@gmail.com>
2024-03-19 07:15:58 +0600

    create model,makemigrations,migrate,create superuser

--- myApp/admin.py
+++ myApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from myApp.models import *
 
-# Register your models here.
+class Customer_User_Display(admin.ModelAdmin):
+    list_display=['display_name','email','User_type']
+
+admin.site.register(Custom_User,Customer_User_Display)
--- myApp/models.py
+++ myApp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Custom_User(AbstractUser):
+    USER=[
+        ('recruiter','Recruiter'),('jobseeker','JobSeeker')
+    ]
+    display_name = models.CharField(max_length=100)
+    email=models.EmailField(max_length=100)
+    password=models.CharField(max_length=100)
+    confirm_password=models.CharField(max_length=100)
+    User_type=models.CharField(choices=USER,max_length=120)
+
+    def __str__(self):
+        return self.display_name
+
